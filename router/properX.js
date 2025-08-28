638656b419be87c3e51df94e79352df846260996
code-shub09 <singhshubham6135@gmail.com>
2025-08-28 20:33:37 +0530

    ok

--- router/properX.js
+++ router/properX.js
@@ -1,32 +1,9 @@
-// const express = require("express");
-// const router = express.Router();
-// const { handlerSend } = require("../controller/sample");
-// const multer = require("multer");
-// const path = require("path");
-
-// const { upload } = require("../config/cloudinary");
-
-// // Multer storage config
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => cb(null, "uploads/"),
-//     filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname))
-// });
-
-// const upload = multer({ storage });
-
-// // POST route with file upload
-// router.post("/sell", upload.array("photos", 20), handlerSend);
-
-// module.exports = router;
-
-
-// // const { upload } = require("../config/cloudinary");
 
 
 const express = require("express");
 const router = express.Router();
 const { handlerSend } = require("../controller/sample");
-const { upload } = require("../config/cloudinary"); // use cloudinary setup
+const { upload } = require("../config/cloud"); // use cloudinary setup
 
 // POST route with file upload
 router.post("/sell", upload.array("photos", 20), handlerSend);
